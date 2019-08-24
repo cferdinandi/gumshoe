7633fec409affa48829c06f1b77c3dcf98e2cd59
Fraser Thompson <fraserthompson0@gmail.com>
2019-08-24 15:26:41 +1200

    Update gumshoe.js

--- src/js/gumshoe/gumshoe.js
+++ src/js/gumshoe/gumshoe.js
@@ -172,7 +172,7 @@
 	 * @return {Boolean}         If true, use the last item
 	 */
 	var useLastItem = function (item, settings) {
-		if (isAtBottom() && isInView(item.content, settings, true)) return true;
+		if (isAtBottom() && item && isInView(item.content, settings, true)) return true;
 		return false;
 	};
 
